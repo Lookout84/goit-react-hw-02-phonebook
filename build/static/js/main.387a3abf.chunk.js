c555ed4a6b58b5703b35bb622164cab5a63ad6e5
Oleksandr Ivashchuk <67271891+Lookout84@users.noreply.github.com>
2021-03-16 19:44:08 +0200

    update

--- build/static/js/main.387a3abf.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-02-phonebook"]=this["webpackJsonpgoit-react-hw-02-phonebook"]||[]).push([[0],{12:function(e,t,n){},13:function(e,t,n){"use strict";n.r(t);var o=n(1),c=n.n(o),r=n(3),i=n.n(r),s=(n(8),n(9),n(0));var h=function(){return Object(s.jsx)("div",{className:"App",children:Object(s.jsx)("h1",{children:"Home work #2_2"})})};n(11),n(12);i.a.render(Object(s.jsx)(c.a.StrictMode,{children:Object(s.jsx)(h,{})}),document.getElementById("root"))},8:function(e,t,n){},9:function(e,t,n){}},[[13,1,2]]]);
-//# sourceMappingURL=main.387a3abf.chunk.js.map
